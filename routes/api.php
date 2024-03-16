5dcfc9852e59f1c771624732aed0c169f2f4086f
kareem-sharaf <151177267+kareem-sharaf@users.noreply.github.com>
2024-03-16 13:02:56 +0300

    .

--- routes/api.php
+++ routes/api.php
@@ -76,3 +76,58 @@ Route::group(['prefix' => 'ad'], function () {
         Route::post('store', 'store');
     });
 });
+
+
+
+Route::group(['prefix' => 'subject'], function () {
+    Route::controller(SubjectController::class)->group(function () {
+        Route::post('show_all_subjects', 'show_all_subjects');
+        Route::post('search_to_subject', 'search_to_subject');
+
+        Route::group(['middleware' => 'auth:sanctum'], function () {
+            Route::post('add_subject', 'add_subject');
+            Route::post('add_subject_and_assign_teachers', 'add_subject_and_assign_teachers');
+            Route::post('edit_subject/{subject_id}', 'edit_subject');
+            Route::delete('delete_subject/{subject_id}', 'delete_subject');
+
+        });
+    });
+});
+
+
+
+Route::group(['prefix' => 'unit'], function () {
+    Route::controller(UnitsController::class)->group(function () {
+        Route::post('show_all_units', 'show_all_units');
+        Route::post('search_to_unit', 'search_to_unit');
+
+        Route::group(['middleware' => 'auth:sanctum'], function () {
+            Route::post('add_unit', 'add_unit');
+            Route::post('edit_unit/{unit_id}', 'edit_unit');
+            Route::delete('delete_unit/{unit_id}', 'delete_unit');
+
+        });
+    });
+});
+
+
+
+
+Route::group(['prefix' => 'teacher'], function () {
+    Route::controller(TeachersController::class)->group(function () {
+        Route::get('show_one_teacher/{teacher_id}', 'show_one_teacher');
+        Route::post('show_all_teachers', 'show_all_teachers');
+        Route::get('show_stage_teachers/{stage_id}', 'show_stage_teachers');
+        Route::get('show_year_teachers/{year_id}', 'show_year_teachers');
+
+        Route::post('search_to_teacher', 'search_to_teacher');
+
+        Route::group(['middleware' => 'auth:sanctum'], function () {
+            Route::post('add_teacher', 'add_teacher');
+            Route::post('add_teacher_and_assign_subjects', 'add_teacher_and_assign_subjects');
+            Route::post('edit_teacher/{teacher_id}', 'edit_teacher');
+            Route::delete('delete_teacher/{teacher_id}', 'delete_teacher');
+
+        });
+    });
+});
