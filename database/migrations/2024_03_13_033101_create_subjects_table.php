00092d44be92dc5af6087c81efa320223dfd6f9a
kareem-sharaf <151177267+kareem-sharaf@users.noreply.github.com>
2024-03-26 13:11:54 +0300

    Update 2024_03_13_033101_create_subjects_table.php

--- database/migrations/2024_03_13_033101_create_subjects_table.php
+++ database/migrations/2024_03_13_033101_create_subjects_table.php
@@ -14,15 +14,10 @@ return new class extends Migration
         Schema::create('subjects', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->binary('image_data')->default('default_image.jpg')->nullable();
-            // $table->unsignedBigInteger('stage_id')->references('id')->on('stages')->onDelete('cascade');
-            // $table->unsignedBigInteger('year_id')->references('id')->on('years')->onDelete('cascade');
-            // $table->unique(['name', 'year_id']);
-
-
-            // بما اني عملت الكورس اضطريت ضيف عندك هون... أسفة :v
-
-            // $table->foreignId('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->integer('price');
+            // $table->binary('image_data')->default('default_image.jpg')->nullable();
+            // $table->unsignedBigInteger('video_id')->nullable()->references('id')->on('videos')->onDelete('cascade');
+            // $table->unsignedBigInteger('file_id')->nullable()->references('id')->on('files')->onDelete('cascade');
 
             $table->timestamps();
         });
