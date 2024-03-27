988904ae2c8130bf680517a9cf7b24cdb9ef0807
Masa Al-Zaeem <125470099+masaalzaeem@users.noreply.github.com>
2024-03-27 14:35:56 +0300

    zzz
    zzz

--- app/Http/Controllers/ClassificationController.php
+++ app/Http/Controllers/ClassificationController.php
@@ -18,15 +18,54 @@ class ClassificationController extends Controller
         );
     }
 
-    //  show the subjects of a specific class
     public function show(Request $request)
     {
-    }
+        $class = Classification::where('class', $request->class)->first();
 
+        if ($class) {
+            return response()->json(['class' => $class], 200);
+        }
+
+        return response()->json(['error' => 'Class not found!'], 404);
+    }
+    //  show the subjects of a specific class
     public function store(Request $request)
     {
+        $request->validate([
+            'class' => 'required|string|unique:classifications',
+        ]);
+
+        $classification = Classification::create([
+            'class' => $request->class,
+        ]);
+
+        return response()->json(
+            [
+                'message' => 'Classification created successfully',
+                'classification' => $classification
+            ],
+            201
+        );
     }
 
+    public function destroy(Request $request)
+    {
+        $classification = Classification::find($request->class_id);
+
+        if (!$classification) {
+            return response()->json(
+                ['error' => 'Classification not found!'],
+                404
+            );
+        }
+
+        $classification->delete();
+
+        return response()->json(
+            ['message' => 'Classification deleted successfully'],
+            200
+        );
+    }
 
     // seed classifications
     public function seedClassification()
