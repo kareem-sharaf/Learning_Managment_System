4eefc24606308960ea5d6809b83e13e6e9b1ee44
kareem-sharaf <151177267+kareem-sharaf@users.noreply.github.com>
2024-06-05 12:41:32 +0300

    .

--- app/Models/Files.php
+++ app/Models/Files.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Files extends Model
 {
     use HasFactory;
-<<<<<<< Updated upstream
     protected $fillable = ['name', 'content', 'subject_id', 'unit_id', 'lesson_id'];
 
 public function subject()
@@ -25,20 +24,10 @@ public function lesson()
 {
     return $this->belongsTo(Lesson::class);
 }
-=======
-    public function unit()
-    {
-        return $this->belongsTo(Unit::class);
-    }
+
     public function leason()
     {
         return $this->belongsTo(Lesson::class);
     }
-    protected $fillable =[
-        'name',
-        'file',
-        'leeson_id',
-        'unit_id',
-    ];
->>>>>>> Stashed changes
+    
 }
